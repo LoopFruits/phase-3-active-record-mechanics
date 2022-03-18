e0bd8852401d3c038091f95ccae8969818c45239
LoopFruits <Kensworldc@gmail.com>
2022-03-18 11:17:08 -0400

    coded along

--- active_record.rb
+++ active_record.rb
@@ -1,7 +1,7 @@
 require "pry"
 require "active_record"
 
-# Setup a database connection
+# Setup a database connection, once establish_connection is run, ActiveRecord::Base keeps it stored as a class variable at ActiveRecord::Base.connection
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "db/students.sqlite"
@@ -19,7 +19,7 @@ ActiveRecord::Base.connection.execute(sql)
 # Log SQL output to the terminal
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
-# Have the Student class inherit from ActiveRecord::Base
+# Have the Student class inherit from ActiveRecord::Base, telling out calss to make use of Active Record's built in ORM  methods. We're also making our class(student) a subclass of ActiveRecord::Base.
 class Student < ActiveRecord::Base
 end
 
